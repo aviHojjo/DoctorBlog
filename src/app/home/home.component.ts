d66088aed955bd1606ffdeeaefbe847b9d7606d7
aviHojjo <avinashpoddar720@gmail.com>
2021-04-23 08:33:47 +0530

    pushing changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,25 +10,45 @@ import { ApiService } from '../api.service';
 export class HomeComponent implements OnInit {
 
   latest: any;
+  orignalTher=[];
+  filteredTher=[];
+  isChecked=false;
 
   constructor(
     private apiService: ApiService,
   ) { }
-
+ 
+  
   ngOnInit(): void {
     this.getLatest();
   }
 
+  checkValue(event: any,val){
+    console.log(event);
+    console.log("value ==>",val);
+ }
   getLatest(): void {
 
     this.apiService.getLatestReleases()
     .subscribe((latest) => {
 
-      console.log(
-        'data =====> ',latest);
+      // console.log(
+      //   'data =====> ',latest);
 
        this.latest = latest;
+       this.latest.map((lat)=>{
+         lat.therapeutics.map((ther)=>{
+           this.orignalTher.push(ther.name)
+         })
+       });
 
     });
+
+    // console.log("whole data ===>",this.orignalTher);
+    // if(this.orignalTher){
+    //   console.log("real     jsjsjs data ===>",this.orignalTher);
+    //   // this.orignalTher
+    // console.log("filtered data ===>",Array.from(new Set(this.orignalTher)))
+    // }
   }
 }
