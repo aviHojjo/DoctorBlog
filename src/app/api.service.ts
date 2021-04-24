d883e945d759f0dbaf9fd01328a60559f052c832
aviHojjo <avinashpoddar720@gmail.com>
2021-04-24 11:34:26 +0530

    changes in code

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -21,32 +21,8 @@ export class ApiService {
   ) { }
   getLatestData(): Observable<any[]> {
 
-    return this.http.get<any[]>(`http://sundewlabs.com/docplexus/api/`) .pipe(
-      tap(_ => console.log('fetched trending')),
-      catchError(this.handleError<any[]>('getLatestReleases', []))
-    );
- 
-  
+    return this.http.get<any[]>(`http://sundewlabs.com/docplexus/api/`);
+    
   }
-   /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-    private handleError<T>(operation = 'operation', result?: T) {
-      return (error: any): Observable<T> => {
-  
-        // TODO: send the error to remote logging infrastructure
-        console.error(error); // log to console instead
-  
-        // TODO: better job of transforming error for user consumption
-        console.log(`${operation} failed: ${error.message}`);
-  
-        // Let the app keep running by returning an empty result.
-        return of(result as T);
-      };
-    }
-
 
 }
